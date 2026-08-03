cf984cc6700967f61cbd1a4b7c8bf84c980fc5b2
Matthew Raison <matthewraison@gmail.com>
2026-08-03 14:53:54 +1000

    Use layer manager and MGA grid enhancements

--- src/map-planner-enhanced.js
+++ src/map-planner-enhanced.js
@@ -1,5 +1,5 @@
 import { createMapPlanner as createBaseMapPlanner } from "./map-planner-v2.js";
-import { enhanceMapPlanner } from "./map-enhancements.js";
+import { enhanceMapPlanner } from "./map-enhancements-v2.js";
 
 export function createMapPlanner(options = {}) {
   const planner = createBaseMapPlanner(options);
