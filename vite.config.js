2df3b2f0f58e2e2fa7bebf7c82172d3d2449b584
Matthew Raison <matthewraison@gmail.com>
2026-08-03 13:03:43 +1000

    Load map reliability bootstrap before application

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [
+    {
+      name: "fieldwork-map-bootstrap",
+      transformIndexHtml(html) {
+        return html.replace(
+          '<script type="module" src="/src/app.js"></script>',
+          '<script type="module" src="/src/map-bootstrap.js"></script>\n    <script type="module" src="/src/app.js"></script>',
+        );
+      },
+    },
+  ],
   build: {
     outDir: "dist",
     sourcemap: false,
