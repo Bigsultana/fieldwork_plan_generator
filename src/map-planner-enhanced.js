742ce02ea2eaaed954ff81b39dba9e51399ecaaf
Matthew Raison <matthewraison@gmail.com>
2026-08-03 15:00:02 +1000

    Compose proven overlays with independent layer manager

--- src/map-planner-enhanced.js
+++ src/map-planner-enhanced.js
@@ -1,7 +1,9 @@
 import { createMapPlanner as createBaseMapPlanner } from "./map-planner-v2.js";
-import { enhanceMapPlanner } from "./map-enhancements-v2.js";
+import { enhanceMapPlanner } from "./map-enhancements.js";
+import { enhanceMapLayers } from "./map-layer-upgrade.js";
 
 export function createMapPlanner(options = {}) {
   const planner = createBaseMapPlanner(options);
-  return enhanceMapPlanner(planner, options);
+  const withOverlays = enhanceMapPlanner(planner, options);
+  return enhanceMapLayers(withOverlays, options);
 }
