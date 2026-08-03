6eb9a0ae86e3b391087511b45e2f54fdb7c604ec
Matthew Raison <matthewraison@gmail.com>
2026-08-03 16:00:31 +1000

    Add precise map framing controls

--- src/map-planner-enhanced.js
+++ src/map-planner-enhanced.js
@@ -1,9 +1,11 @@
 import { createMapPlanner as createBaseMapPlanner } from "./map-planner-v2.js";
 import { enhanceMapPlanner } from "./map-enhancements.js";
 import { enhanceMapLayers } from "./map-layer-upgrade.js";
+import { enhanceMapViewControls } from "./map-view-controls.js";
 
 export function createMapPlanner(options = {}) {
   const planner = createBaseMapPlanner(options);
   const withOverlays = enhanceMapPlanner(planner, options);
-  return enhanceMapLayers(withOverlays, options);
+  const withLayers = enhanceMapLayers(withOverlays, options);
+  return enhanceMapViewControls(withLayers, options);
 }
